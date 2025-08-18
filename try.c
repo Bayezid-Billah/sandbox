96bc1de20d6a60975d86554293ebabec3931ae87
bayezid <bayezidbillah@gmail.com>
2025-08-19 02:10:25 +0600

    Initial Commit

---
+++ try.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello world my man!\n");
+	return 0;
+}
---
+++ try.py
@@ -0,0 +1,18 @@
+mystring = 'Hello my'
+print(mystring + ' frendo')
+mylist = []
+mylist.append(1)
+mylist.append(5)
+print(mylist)
+print("The added string is done. %s" %mystring)
+
+number = 1+2 * 5/2
+print(number)
+print(20%number)
+helloworld = "hello" + " \n " + "world"
+print(helloworld)
+print(helloworld * 10)
+name = "Kodu"
+print("Hello, %s" % name)
+age = 11
+print("%s is %d years old." %(name, age))
