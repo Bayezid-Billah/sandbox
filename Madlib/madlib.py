1907e82028db9f5725b64576a6e6fc63eff5e1ea
Bayezid Billah <bayezidbillah@gmail.com>
2025-08-22 18:34:00 +0600

    madlibs

---
+++ Madlib/madlib.py
@@ -0,0 +1,11 @@
+
+
+
+adj1=input("Adjective: ")
+adj2=input("Adjective: ")
+noun1=input("Noun: ")
+famous_person=input("Famous person: ")
+
+madlib= f"Hello there {adj1} person! Hope that you are {adj2}, I am here trying to make \n a madlib for practicing python. If you are reading this, I believe {noun1} \n is trying to check out if my code runs! This is most likely to result \n in me getting an opportunity to aquire some attention. A lot of famous \n people such as {famous_person} started their career thanks to programming and engineering \n and stuff. If I keep practicing I hope one day I will be able to earn \n my own bread too."
+
+print(madlib)
