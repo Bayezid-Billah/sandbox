7a69ae1e10ec16de26fec9604e00581821e89d8f
Bayezid Billah <bayezidbillah@gmail.com>
2025-09-01 15:00:57 +0600

    fetch

--- Guess_number/guess_number_binary_search.py
+++ Guess_number/guess_number_binary_search.py
@@ -1,16 +1,18 @@
 
 def guess(low,high):
-
+    iteration = 0
     status = ""
     feedback =""
     print(f"Welcome! I'll be guessing your number between {low} and {high} ! ")
 
     while status != "c":
-        status = input(f"Is your number "+str(round(((high - low)/2+low)))+"\n (Higher: H, Lower: L, Correct: C):").lower()
+        iteration +=1
+        status = input(f"Iteration: {iteration} \nIs your number "+str(round(((high - low)/2+low)))+"\n (Higher: H, Lower: L, Correct: C):").lower()
         if status == "h":
             low = (high - low)/2 +  low
         elif status == "l":
             high = (high - low)/2 + low
+            
 
     print("Yay! I guessed the number  Correctly!")
     
